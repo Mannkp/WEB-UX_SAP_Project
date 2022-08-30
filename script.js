d44fff76cca28d42a446bfc9ea8ee3c8dac79933
Mannkumar Pandya <mannkpandya@gmail.com>
2022-08-30 19:12:50 +0530

    JS practice

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ function onVisitPage()
 {
     alert("Welcome to Mann's Webpage")
 }   
+
+let fruits = ['apple' , 'banana']
+console.log(fruits.length);
+
+console.log(fruits.indexOf('banana'));
+
+fruits.push('Mango');
+
+console.log(fruits);
+
+fruits.pop()    //it will pop(delete) last element from array
+
+//fruits.splice() -> used when we know the index of element to be deleted
