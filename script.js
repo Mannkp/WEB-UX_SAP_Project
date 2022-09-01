2f95af6fc3e81c85e870692df169d2993b72c7a8
Mannkumar Pandya <mannkpandya@gmail.com>
2022-09-01 22:58:40 +0530

    Practiced JS

--- script.js
+++ script.js
@@ -15,3 +15,15 @@ console.log(fruits);
 fruits.pop()    //it will pop(delete) last element from array
 
 //fruits.splice() -> used when we know the index of element to be deleted
+
+let n = 5;
+for(let i=0;i<=n;i++){
+    console.log('Hii JavaScript!');
+}
+
+let i=0;
+
+while(i<=n){
+    console.log('Mann is Learning Java Script : )');
+    i++;
+}
